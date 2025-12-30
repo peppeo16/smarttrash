e3f6fdd291f1d05f7bf5345013bf66b9e939cf9c
peppeo16 <g.ordine@studenti.poliba.it>
2025-12-30 16:12:36 +0100

    Collegamento network funzionante

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     formData.append("file", selectedFile);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("http://192.168.1.106:8000/predict", {
         method: "POST",
         body: formData,
       });
